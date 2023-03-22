39811a4d4ffec9113e63c865a0783a42f62360ae
kbeason <kevin.beason@dreamworks.com>
2023-03-22 11:12:55 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '10.2'
+    _version = '10.3'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
