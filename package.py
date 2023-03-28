9fc59c406f5a7163f21c31959eda9b64c8ae31bb
rwoods <ron.woods@dreamworks.com>
2023-03-28 16:00:22 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '10.3'
+    _version = '10.4'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
